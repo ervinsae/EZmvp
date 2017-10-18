009d35c4330d502e512201243c72e6fecf0e4bf6
Ervin <ervinsae@gmail.com>
2017-10-18 19:48:32 +0800

    1，create github

--- app/src/main/java/android/ervin/mvp/MainActivity.java
+++ app/src/main/java/android/ervin/mvp/MainActivity.java
@@ -1,7 +1,8 @@
 package android.ervin.mvp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("main","------------");
     }
 }
