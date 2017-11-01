04393c20f1ffc30d66a2a02d17e036852f4b8d2d
Ervin <ervinsae@gmail.com>
2017-11-01 17:54:42 +0800

    1，添加一些工具类（注意其中对图片的解析，url加http前缀才可以（坑1），时间处理） 2，加入自定义Titlebar，沉浸状态栏，需要支持V23

---
+++ app/src/main/java/com/ervin/mvp/utils/DensityHelper.java
@@ -0,0 +1,65 @@
+package com.ervin.mvp.utils;
+
+import android.content.Context;
+import android.view.WindowManager;
+
+/**
+ * Created by think on 2016/4/22.
+ */
+public class DensityHelper {
+    /**
+     * 根据手机的分辨率从 dip 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp==dip
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 将px值转换为sp值，保证文字大小不变
+     *
+     * @param pxValue
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float	fontScale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * 将sp值转换为px值，保证文字大小不变
+     *
+     * @param spValue
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().density;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    public static int displayWidth(Context context){
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+
+        int width = wm.getDefaultDisplay().getWidth();
+        return width;
+
+    }
+    public static int displayHeight(Context context){
+        WindowManager wm = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+
+        int height = wm.getDefaultDisplay().getHeight();
+        return height;
+
+    }
+
+}
